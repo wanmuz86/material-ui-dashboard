1e75006a0d5b0b4aa21d74e4c8869a3fc5ca9b93
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2020-03-31 19:33:31 +0800

    add item

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
 import Main from '../components/Main'
+import Copyright from '../components/Copyright'
 class Dashboard extends React.Component {
 
     constructor(){
@@ -14,6 +15,7 @@ class Dashboard extends React.Component {
     <main>
     <Main/>
     </main>
+    <Copyright/>
     
 </div>
         );
